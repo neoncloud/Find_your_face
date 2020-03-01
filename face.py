e4de2f4e7cb54e61822d02849fe1c0f552a0e467
Neoncloud <Neoncloud2119@gmail.com>
2020-03-01 12:10:34 +0800

    Revert "Add headers (User agent) to the request"
    This reverts commit e079d3326ce79d5b48b0549a8b6eca35973047f1.

--- face.py
+++ face.py
@@ -6,16 +6,16 @@ import json
 import time
 
 #constants
-face_url = 'https://thispersondoesnotexist.com/image'
+face_url = 'https://thispersondoesnotexist.com/'
 counter = 0
 best_so_far = 0
-headers = {'User-Agent' : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"}
+
 #arguments
 parser = argparse.ArgumentParser(description="Find your face from the void.",epilog="You should apply for the api access from Face++, see more help in README.")
 parser.add_argument("-m","--my_face_file", required = True, help = "Path to your face image file")
 parser.add_argument("-k","--api_key", required = True, help = "Your api_key")
 parser.add_argument("-s","--api_secret", required = True, help = "Your api_secret")
-parser.add_argument("-c","--confidence", type = float, default = 80, help = "Set the similarity of your face and the faces you want to save")
+parser.add_argument("-c","--confidence", type = float, default = 60, help = "Set the similarity of your face and the faces you want to save")
 parser.add_argument("-n","--how_many", type = int, default = 10, help = "Set the quantity of the faces you want to save")
 
 args = parser.parse_args()
@@ -38,9 +38,9 @@ face_set_post = {'api_key':api_key,
                 'face_tokens':my_face_dict['faces'][0]['face_token']
                 }
 requests.post('https://api-cn.faceplusplus.com/facepp/v3/faceset/create', data = face_set_post)
-print(my_face_dict['faces'][0]['face_token'])
+
 while 1:
-    face_image = requests.get(self.face_url,headers= self.headers)
+    face_image = requests.get(face_url)
     face_image_file = {'image_file2':('face',face_image.content)}   #post img file
     post_data = {'api_key':api_key, 
                 'api_secret':api_secret,
